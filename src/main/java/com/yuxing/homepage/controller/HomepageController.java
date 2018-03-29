5529e031192b1509e9d13eedfdb8095d8e28e9ca
jsun999 <jsun999@126.com>
2018-03-29 21:29:42 +0800

    去除index.do

--- src/main/java/com/yuxing/homepage/controller/HomepageController.java
+++ src/main/java/com/yuxing/homepage/controller/HomepageController.java
@@ -8,10 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomepageController {
 	
-	@RequestMapping("index.do")
-	public String index(){
-		return "views/introduce.html";
-	}
 	@RequestMapping("contactus.do")
 	public String contactus(){
 		return "views/contactus.html";
